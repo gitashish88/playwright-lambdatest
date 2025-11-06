db39280c2af793628b6459e2bb887f2950261ded
Ashish Sharma <a.r.sharma111@gmail.com>
2025-11-07 01:05:50 +0530

    created.env file and changed headless property

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,8 +24,10 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+ 
+
   use: {
-    headless: false,
+    headless: process.env.CI ? true : false,
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
