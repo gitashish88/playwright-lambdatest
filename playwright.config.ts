dabd4e9408d0b7198288563df3e1c795e09055bc
Ashish Sharma <a.r.sharma111@gmail.com>
2025-11-08 22:48:23 +0530

    capabilities

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,31 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';        //npm install dotenv --save-dev
+
+//lambdatest-capabilities
+const capabilities = {
+    browserName: "Chrome",
+    browserVersion: "latest",
+    "LT:Options": {
+        platform: "WINDOWS 10",
+        build: "Playwright Test from config",
+        name: "Playwright Test - Config File",
+        user: "arsharma111",
+        accessKey: "LT_7odLgeLge64DAjA29czpWghXmy208Iw3RcPPJFAdy67DJ7I",
+        network: true,
+        video: true,
+        console: true,
+        tunnel: false,
+        tunnelName: "",
+        geoLocation: "",
+    },
+};
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+// Read from ".env" file
+dotenv.config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -29,6 +48,12 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
  
   use: {
+
+    connectOptions: {
+      wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=
+            ${encodeURIComponent(JSON.stringify(capabilities))}`,
+    },
+    baseURL: process.env.BASE_URL || 'https://ecommerce-playground.lambdatest.io/', // Example usage
     headless: process.env.CI ? true : false,
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
@@ -46,15 +71,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
--- tests/context-demo.spec.ts
+++ tests/context-demo.spec.ts
@@ -1,34 +1,40 @@
 import { test, expect, chromium } from '@playwright/test';
 
+//lambdatest-capabilities
+const capabilities = {
+    browserName: "Chrome",
+    browserVersion: "latest",
+    "LT:Options": {
+        platform: "WINDOWS 10",
+        build: "Playwright Test Build",
+        name: "Playwright Test",
+        user: "arsharma111",
+        accessKey: "LT_7odLgeLge64DAjA29czpWghXmy208Iw3RcPPJFAdy67DJ7I",
+        network: true,
+        video: true,
+        console: true,
+        tunnel: false,
+        tunnelName: "",
+        geoLocation: "",
+    },
+};
+
 test.describe('Demo for test context', () => {
 
     test('open the browser and set context', async () => {
 
-        const browser = await chromium.launch();
+        const browser = await chromium.connect(`wss://cdp.lambdatest.com/playwright?capabilities=
+            ${encodeURIComponent(JSON.stringify(capabilities))}`);
         const context = await browser.newContext();
         const page = await context.newPage();
 
-        await page.goto('https://ecommerce-playground.lambdatest.io/');
+        await page.goto(process.env.URL || '');
         await page.hover("a[data-toggle='dropdown'] span:has-text('My account')");
         await page.click("span:has-text('Login')");
         await page.fill("input[name='email']", "a.r.sharma111@gmail.com");
         await page.fill("input[name='password']", "ashish@88");
         await page.click("input[value='Login']");
-        await page.waitForLoadState('networkidle');
-        await page.hover("a[data-toggle='dropdown'] span:has-text('My account')");
-        await page.click('span:has-text("Logout")');
-        
-        // const page1 = await context.newPage();
-        // await page1.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/login');
-
-        // const newContext = await browser.newContext();
-        // const newPage = await newContext.newPage();
-        // await newPage.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/login');
-
-        // newPage.close();
-        // page1.close();
-
-        page.close();
+        await page.close();
         await browser.close();
     })
 })
